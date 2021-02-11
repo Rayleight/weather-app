62a9916a2ea15fd6c37d0be21c44d9e2f87b05cd
renarldomason@gmail.com <you@example.com>
2021-02-11 17:39:53 -0500

    Updated APIUX method

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ApixuService } from "../apixu.service";
 
 @Component({
   selector: 'app-weather',
@@ -9,7 +10,9 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class WeatherComponent implements OnInit {
   public weatherSearchForm: FormGroup;
   
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder,
+    private apixuService: ApixuService
+    ) {}
 
   ngOnInit(): void {
     this.weatherSearchForm = this.formBuilder.group({
@@ -17,7 +20,9 @@ export class WeatherComponent implements OnInit {
     });
   }
   sendToAPIXU(formValues) {
-    console.log(formValues);
+    this.apixuService
+      .getWeather(formValues.location)
+      .subscribe(data => console.log(data));
   }
   
   
