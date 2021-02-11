8d988c86de34aa9c43936c943035128a027f1db5
renarldomason@gmail.com <you@example.com>
2021-02-11 12:02:29 -0500

    added module components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { WeatherComponent } from './weather/weather.component';
 import {RouterModule} from '@angular/router';
 import {allAppRoutes} from './routes';
-
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,6 +15,7 @@ import {allAppRoutes} from './routes';
   imports: [
     BrowserModule,
     RouterModule.forRoot(allAppRoutes)
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
