71b5a09a106aaef8e471cf7a9aa0fb68a101863f
renarldomason@gmail.com <you@example.com>
2021-02-11 23:41:37 -0500

    modified script to use weather app with location

--- src/app/apixu.service.ts
+++ src/app/apixu.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+const urlKey = '34345a4caf202c468ae8f11318c4d460&';
+const urlUnits = 'f'; // use m for metrics
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,7 +12,7 @@ export class ApixuService {
   constructor(private http: HttpClient) {}
   getWeather(location){
     return this.http.get(
-        'https://api.apixu.com/v1/current.json?key=34345a4caf202c468ae8f11318c4d460&q=' + location
+      'http://api.weatherstack.com/current?access_key=' + urlKey + '&units=' + urlUnits + '&query=' + location
     );
 }
 }
